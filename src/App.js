41e36167b238be3a88fafe9b6237d81409b65505
Thayanne Soares <133281353+MisterEu09@users.noreply.github.com>
2025-02-01 19:31:47 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,14 @@ function App() {
       <Playlist/>
       <Footer/>
     </>
-
+    );
+    ReactDOM.render(
+      <React.StrictMode>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </React.StrictMode>,
+      document.getElementById('root')
     );
 }
 
